95b8a69b71bf37c1a8853b082de1d9aae8b1559b
Yash <yashkhatri88540@gmail.com>
2026-05-08 23:02:59 +0530

    cors1

--- server/src/app.js
+++ server/src/app.js
@@ -10,13 +10,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://google67.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://google67.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.options("*", cors());
+
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
